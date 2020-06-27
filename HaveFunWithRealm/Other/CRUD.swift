123efaaae4d779a05af97d82938ad5c0e4d259e0
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-06-27 16:14:07 +0430

    Added CRUD enum

---
+++ HaveFunWithRealm/Other/CRUD.swift
@@ -0,0 +1,18 @@
+//
+//  CRUD.swift
+//  HaveFunWithRealm
+//
+//  Created by NP2 on 6/27/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import Foundation
+
+public enum CRUD: UInt8 {
+    
+    case create
+    case read
+    case update
+    case delete
+    
+}
