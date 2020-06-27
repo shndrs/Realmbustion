40f3ab53a16a7ea3ca26f2815ce5b8175a8fa0ea
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-06-27 16:17:22 +0430

    Added Strings

---
+++ HaveFunWithRealm/Other/Strings.swift
@@ -0,0 +1,15 @@
+//
+//  Strings.swift
+//  HaveFunWithRealm
+//
+//  Created by NP2 on 6/27/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import Foundation
+
+enum Strings: String {
+    
+    case updateError = "DBError: 'newItem' must has a value for update action"
+    
+}
