4645d2760e764f64aab27f515fb8a7ae31204611
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-06-27 16:24:21 +0430

    Relocate example files to EX dir

---
+++ HaveFunWithRealm/EX/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HaveFunWithRealm
+//
+//  Created by Sahand Raeisi on 1/6/19.
+//  Copyright © 2019 Sahand Raeisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
