be14010ff567c39923d5582e9640116687c89ce2
Sahand Raeisi <sahandraeisi1994@gmail.com>
2020-06-27 16:16:31 +0430

    Typealias added

---
+++ HaveFunWithRealm/Other/Typealias.swift
@@ -0,0 +1,14 @@
+//
+//  Typealias.swift
+//  HaveFunWithRealm
+//
+//  Created by NP2 on 6/27/20.
+//  Copyright © 2020 Sahand Raeisi. All rights reserved.
+//
+
+import Foundation
+
+/// - Note: (success: Bool, error: Error?)
+
+public typealias DBCallback = (Bool, Error?) -> Void
+public typealias Action = () -> Void
